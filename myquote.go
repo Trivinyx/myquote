47021ae1179fcb7fb11fdba59013acfdff7e436c
Trivinyx <tom.a.s.myre@gmail.com>
2022-02-02 23:50:57 +0100

    initial commit
    added go.mod, added go.sum, added myquote.go

---
+++ myquote.go
@@ -0,0 +1,20 @@
+package myquote
+
+import "rsc.io/quote/v4"
+import "fmt"
+
+func Glass() {
+	fmt.println(quote.Glass())
+}
+
+func GO() {
+	fmt.println(quote.Go())
+}
+
+func Hello() {
+	fmt.println(quote.Hello())
+}
+
+func Opt() {
+	fmt.println(quote.Opt())
+}
